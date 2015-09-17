79f71d13c8ddec47e762412627993a86e5334474
Jianru Lin <jianru.lin@gmail.com>
2015-09-17 21:11:14 +0800

    测试客户端

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ exports.udp_echo_server = function(opt) {
     s.bind(opt.port, opt.host)
 }
 
-exports.udp_robot = function(opt, cb) {
+exports.test_udp_echo_server = function(opt) {
+    var s = dgram.createSocket('udp4');
+    var id = 0
 
+    s.on('error', function(err) {
+        console.log(err.toString())
+    })
+
+    s.on('listening', function() {
+        var address = s.address()
+        console.log('listening ip=' + address.address + ' port=' + address.port)
+        send_next()
+    })
+
+    s.on('message', function(msg, rinfo) {
+        console.log('receive from ip=' + rinfo.address + ' port=' + rinfo.port + ' content=' + JSON.stringify(msg.toString()))
+        setTimeout(send_next, 1000)
+    })
+
+    s.bind()
+
+    function send_next() {
+        var msg = new Buffer('hello ' + (id++))
+        console.log('send to host=' + opt.host + ' port=' + opt.port + ' content=' + JSON.stringify(msg.toString()))
+        s.send(msg, 0, msg.length, opt.port, opt.host)  
+    }
 }
