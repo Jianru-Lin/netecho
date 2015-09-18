22eb41b90bf90c421e732925101e899dcbad4ecf
Jianru Lin <jianru.lin@gmail.com>
2015-09-18 21:23:10 +0800

    增加了 tcp 回应服务功能

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var dgram = require('dgram')
+var net = require('net')
 
 exports.udp_echo_server = function(opt) {
     var s = dgram.createSocket('udp4');
 
     s.on('error', function(err) {
-        console.log(err.toString())
+        console.log('error: ' + err.message)
     })
 
     s.on('listening', function() {
@@ -25,7 +26,7 @@ exports.test_udp_echo_server = function(opt) {
     var id = 0
 
     s.on('error', function(err) {
-        console.log(err.toString())
+        console.log('error: ' + err.message)
     })
 
     s.on('listening', function() {
@@ -45,5 +46,71 @@ exports.test_udp_echo_server = function(opt) {
         var msg = new Buffer('hello ' + (id++))
         console.log('send to host=' + opt.host + ' port=' + opt.port + ' content=' + JSON.stringify(msg.toString()))
         s.send(msg, 0, msg.length, opt.port, opt.host)  
+    }
+}
+
+exports.tcp_echo_server = function(opt) {
+    var s = net.createServer()
+
+    s.on('error', function(err) {
+        console.log('error: ' + err.message)
+    })
+
+    s.on('listening', function() {
+        var address = s.address()
+        console.log('listening ip=' + address.address + ' port=' + address.port)
+    })
+
+    s.on('connection', function(client) {
+        var address = client.address()
+        console.log('connection from ip=' + address.address + ' port=' + address.port)
+
+        client.on('data', function(chunk) {
+            console.log('client data length=' + chunk.length)
+            client.write(chunk)
+        })
+
+        client.on('close', function() {
+            console.log('client close')
+        })
+
+        client.on('error', function(err) {
+            console.log('client error: ' + err.message)
+        })
+    })
+
+    s.on('close', function() {
+        console.log('close')
+    })
+
+    s.listen(opt.port, opt.host)
+}
+
+exports.test_tcp_echo_server = function(opt) {
+    var s = net.connect(opt.port, opt.host)
+    var id = 0
+
+    s.on('error', function(err) {
+        console.log('error: ' + err.message)
+    })
+
+    s.on('connect', function() {
+        console.log('connect ip=' + s.remoteAddress + ' port=' + s.remotePort)
+        send_next()
+    })
+
+    s.on('data', function(chunk) {
+        console.log('receive from ip=' + s.remoteAddress + ' port=' + s.remotePort + 'content=' + JSON.stringify(chunk.toString()))
+        setTimeout(send_next, 1000)
+    })
+
+    s.on('close', function() {
+        
+    })
+
+    function send_next() {
+        var msg = new Buffer('hello ' + (id++))
+        console.log('send to ip=' + s.remoteAddress + ' port=' + s.remotePort + ' content=' + JSON.stringify(msg.toString()))
+        s.write(msg)
     }
 }
